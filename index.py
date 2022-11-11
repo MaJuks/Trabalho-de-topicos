c878e37dc88fec291c58f99552c83c7303c2f47c
Maria Júlia <75435563+MaJuks@users.noreply.github.com>
2022-11-11 19:49:03 -0300

    Update index.py

--- index.py
+++ index.py
@@ -32,73 +32,82 @@ clubes_df = ["America-MG","Athletico-PR","Atletico-GO","Atletico-MG","Avai","Bah
 
 #coletando informações do usuario
 #Clube
-st.write('Escolha um time para a análise')
-clube_user = st.selectbox('opções: ', clubes_df)
-st.write(f'- CLUBE escolhido: {clube_user}')
+st.header('Faremos uma análise de partidas de futebol com base nos dados do usuário!')
+st.write('- ESCOLHA UM TIME PARA A ANÁLISE')
+clube_user = st.selectbox('', clubes_df)
+st.text(f'clube escolhido: {clube_user}')
 vals = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+tester = 0
 for i in range(0,29):
     clube_lower = clubes_df[i].lower()
     clube_user_lower = clube_user.lower()
     if clube_user_lower == clube_lower:
         clube_user2 = clubes_df[i]
         vals[i] = 1
-        st.write('Ok')
+        tester = 1
+if tester == 0:
+    st.write("erro1")
 df_user = df_full[df_full["clube"] == clube_user2]
 #Chutes
-st.write('Insira um número de chutes')
+st.write('- INSIRA UM NÚMERO DE CHUTES')
 chutes_min = int(df_user["chutes"].min())
 chutes_max = int(df_user["chutes"].max())
-chutes_user = st.slider("Selecione: ",chutes_min,chutes_max)
-st.write(f'Chutes: {chutes_user}')
+chutes_user = st.slider("",chutes_min,chutes_max)
+st.text(f'quantidade de chutes selecionados: {chutes_user}')
 #Chutes no alvo
-st.write('Insira um número de chutes no alvo')
+st.write('- INSIRA UM NÚMERO DE CHUTES NO ALVO')
 chutes_alvo_min = int(df_user["chutes_no_alvo"].min())
 chutes_alvo_max = int(df_user["chutes_no_alvo"].max())
-chutes_alvo_user = st.slider("Selecione: ",chutes_alvo_min,chutes_alvo_max)
-st.write(f'Chutes no alvo: {chutes_alvo_user}')
+chutes_alvo_user = st.slider("",chutes_alvo_min,chutes_alvo_max)
+st.text(f'quantidade de chutes no alvo selecionado: {chutes_alvo_user}')
 #upando resultados
 vals[29] = chutes_user
 vals[30] = chutes_alvo_user
-st.write('Ok2')
-
 #predição
 res = arv.predict([vals])
 resul = str(res[0])
-st.write('De acordo com a analise, seu time nesses parametros possui uma porcentagem de: ',resul," em posse de bola")
-
+st.subheader(f'De acordo com a análise, seu time nesses parametros possui uma porcentagem de: {resul} em posse de bola')
+st.write("Quantidade de partidas em que seu time possuiu essa porcentagem de posse de bola, e seus dados estatísticos")
 #avaliando partidas
 df_user = df_full[df_full["clube"] == clube_user2]
 df_full_resul = df_user[df_user["posse_de_bola"]==resul]
 id_partida = df_full_resul["partida_id"].values
 st.write(df_full_resul)
-
+st.subheader("Agora, faremos uma analise dessas partidas, indicando se dentro desses parametros calculados ele, clube escolhido, venceria, perderia ou ganharia o jogo!")
 #analise final
 empate = 0 
 vitoria = 0
 derrota = 0
+st.write('- PARTIDAS:')
 for i in range(0,len(id_partida)):
-#   print("partida:", i)
-  contador_gols_time = 0
-  contador_gols_rival = 0 
-  partida = df_gols[df_gols["partida_id"] == id_partida[i]]
-#   print(partida)
-  gols = partida["clube"].values
-  for p in range(0,len(gols)):
-    if gols[p] == clube_user2:
-      contador_gols_time +=1 
+    rival_df = df_full[df_full["partida_id"] == id_partida[i]]
+    rival_value = rival_df["clube"].values
+    for k in range(0,len(rival_value)):
+        if clube_user_lower != rival_value[k].lower():
+            rival = rival_value[k]
+    st.text(f'{clube_user2}  x  {rival}')
+    contador_gols_time = 0
+    contador_gols_rival = 0 
+    partida = df_gols[df_gols["partida_id"] == id_partida[i]]
+    st.write(partida)
+    gols = partida["clube"].values
+    for p in range(0,len(gols)):
+        if gols[p] == clube_user2:
+            contador_gols_time +=1 
+        else:
+            contador_gols_rival +=1
+    if contador_gols_time == contador_gols_rival:
+        empate += 1
+        st.write("Resultado empate! ")
+    elif contador_gols_time > contador_gols_rival:
+        vitoria += 1
+        st.write("Resultado vitoria!")
+    elif contador_gols_time < contador_gols_rival:
+        derrota += 1
+        st.write("Resultado derrota!")
     else:
-      contador_gols_rival +=1
-  if contador_gols_time == contador_gols_rival:
-    empate += 1
-    #print("partida", i ,"resultado empate: ", empate)
-  elif contador_gols_time > contador_gols_rival:
-    vitoria += 1
-    #print("partida", i ,"resultado vitoria: ", vitoria)
-  elif contador_gols_time < contador_gols_rival:
-    derrota += 1
-    #print("partida", i ,"resultado derrota: ", derrota)
-  else:
-    st.write('Erro2')
+        st.write('Erro2')
+st.write('- DADOS DAS PARTIDAS:')
 st.write('vitorias: ',vitoria)
 st.write('empate: ',empate)
 st.write('derrota: ',derrota)
