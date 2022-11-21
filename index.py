f7854f49007cede66c959d1d7a8cd879dc0e132f
Maria Julia <mariajnascimento04@gmail.com>
2022-11-21 17:19:17 -0300

    trocando base

--- index.py
+++ index.py
@@ -9,22 +9,23 @@ st.title('Tópicos Especiais em Informática - Trabalho')
 
 # carregando a base
 database1= 'base-teste.csv' #caminho da base de dados
-database2 = 'base-teste2.csv' #caminho da base de dados
-df_esta = pd.read_csv(database1, sep=",") # dataset estatísticas
-df_gols = pd.read_csv(database2, sep=",") # dataset gols
+database3 = 'base-teste3.csv' #caminho da base de dados
+df_estatistica= pd.read_csv(database1, sep=",") # dataset estatísticas
+df_dados = pd.read_csv(database3, sep=",") # dataset dados full
 
-#limpando base estatística (partidas que não aconteceram)  2992 rows × 13 columns
-df_full = df_esta.dropna(subset=df_esta.select_dtypes(int).columns, how='all')
-df_full = df_full[df_full["chutes"] != 0]
-df_full = df_full[df_full["precisao_passes"] != "None"]
+
+#limpando base estatística (partidas que não aconteceram ou muito antigos (poucas informações))  2992 rows × 13 columns
+df_estatistica= df_estatistica.dropna(subset=df_estatistica.select_dtypes(int).columns, how='all')
+df_estatistica= df_estatistica[df_estatistica["chutes"] != 0]
+df_estatistica= df_estatistica[df_estatistica["precisao_passes"] != "None"]
 
 #criando arvore
 quali = ['clube'] #Variáveis qualitativas
 quant = ['chutes', 'chutes_no_alvo'] #Variáveis quantitativas
-dfQualiDummies = pd.get_dummies(df_full[quali]) #Dataframe com qualitativas dummy
-dfQuant = df_full[quant] #Dataframe com quantitativas
+dfQualiDummies = pd.get_dummies(df_estatistica[quali]) #Dataframe com qualitativas dummy
+dfQuant = df_estatistica[quant] #Dataframe com quantitativas
 dfWork = pd.concat([dfQualiDummies, dfQuant ], axis=1 ) #Dataframe com quali dummy e quant
-target = df_full['posse_de_bola'] # variavel alvo da predição (string)
+target = df_estatistica['posse_de_bola'] # variavel alvo da predição (string)
 arv = tree.DecisionTreeClassifier() #árvore de classificação 
 arv.fit(dfWork, target)
 
@@ -46,9 +47,8 @@ for i in range(0,29): # 29 times
         tester = 1
 if tester == 0:
     st.write("erro1")
-df_user = df_full[df_full["clube"] == clube_user] #filtrando base de dados com o clube escolhidp
+df_user = df_estatistica[df_estatistica["clube"] == clube_user] #filtrando base de dados com o clube escolhidp
 # - Chutes - Finalizações
-
 st.write('- INSIRA UM NÚMERO DE CHUTES')
 chutes_min = int(df_user["chutes"].min())
 chutes_max = int(df_user["chutes"].max())
@@ -71,48 +71,42 @@ st.subheader(f'De acordo com a análise, seu time nesses parametros possui uma p
 st.write("Quantidade de partidas em que seu time possuiu essa porcentagem de posse de bola, e seus dados estatísticos")
 
 # Avaliando partidas
-df_full_resul = df_user[df_user["posse_de_bola"]==resul] #coletando partidas do time escolhido com posse de bola predita
-id_partida = df_full_resul["partida_id"].values # id das partidas
-st.write(df_full_resul)
+df_estatistica_resul = df_user[df_user["posse_de_bola"]==resul] #coletando partidas do time escolhido com posse de bola predita
+id_partida = df_estatistica_resul["partida_id"].values # id das partidas
+st.write(df_estatistica_resul)
 st.subheader("Agora, faremos uma analise dessas partidas, indicando se dentro desses parametros calculados ele, clube escolhido, venceria, perderia ou ganharia o jogo!")
 
 #Variáveis de análise final zerados
+st.write('- PARTIDAS:')
+contador = 1 
 empate = 0 
 vitoria = 0
 derrota = 0
-st.write('- PARTIDAS:')
-
-for i in range(0,len(id_partida)): # quantidade de partidas
-    rival_df = df_full[df_full["partida_id"] == id_partida[i]] # procurando o outro time que possui o mesmo id da partida (rival)
-    rival_value = rival_df["clube"].values # salvando nome do time escolhido e time rival (possuem o mesmo id = mesma partida)
-    for k in range(0,len(rival_value)): # 2 times
-        if clube_user != rival_value[k]: 
-            rival = rival_value[k] # coletando nome do clube rival
-    st.text(f'{clube_user}  x  {rival}')
-    #Variáveis para contar gols da partida zerados
-    contador_gols_time = 0
-    contador_gols_rival = 0 
-    partida = df_gols[df_gols["partida_id"] == id_partida[i]] #procurando a partida no dataset de gols
-    st.write(partida)
-    gols = partida["clube"].values #clubes que fizeram o gol nessa partida
-    for p in range(0,len(gols)): # gols
-        if gols[p] == clube_user: # Gols do time escolhido
-            contador_gols_time +=1 
-        else: #gols do rival
-            contador_gols_rival +=1
-    #analisando o resultado da partida
-    if contador_gols_time == contador_gols_rival: #empate
-        empate += 1
-        st.write(f"Resultado empate! {contador_gols_time} x {contador_gols_rival}")
-    elif contador_gols_time > contador_gols_rival: #vitoria
-        vitoria += 1
-        st.write(f"Resultado vitoria! {contador_gols_time} x {contador_gols_rival}")
-    elif contador_gols_time < contador_gols_rival: #derrota
-        derrota += 1
-        st.write(f"Resultado derrota! {contador_gols_time} x {contador_gols_rival}")
+for i in range (0,len(id_partida)): # detalhar cada partida
+    clube_rival = ""
+    pontos_rival = 0
+    partida = df_dados[df_dados["ID"]== id_partida[i]] #filtrando partidas em outra base
+    dados = ['data','mandante',"visitante","vencedor","mandante_Placar","visitante_Placar"] # colunas específicas
+    partida = partida[dados] #filtrando
+    partida_data = partida["data"].values # datando
+    if partida["mandante"].values == clube_user: # identificando mandante e visitante (recolhendo placar)
+        clube_rival = partida["visitante"].values
+        pontos_rival = int(partida["visitante_Placar"].values)
+        pontos_user = int(partida["mandante_Placar"].values)
     else:
-        st.write('Erro2')
-#apresentando resultados
+        clube_rival = partida["mandante"].values
+        pontos_rival = int(partida["mandante_Placar"].values)
+        pontos_user = int(partida["visitante_Placar"].values)
+    if partida["vencedor"].values == clube_user: # identificando vencedor
+        vitoria += 1
+    elif partida["vencedor"].values == "-":
+        empate += 1 
+    else:
+        derrota += 1 
+    st.write(f"{contador} - {partida_data[0]} --- {clube_user} {pontos_user} x {pontos_rival} {clube_rival[0]}") #apresentando dados
+    contador +=1
+
+#apresentando dados
 st.write('- DADOS DAS PARTIDAS:')
 st.write('vitorias: ',vitoria)
 st.write('empate: ',empate)
@@ -121,9 +115,9 @@ st.write('derrota: ',derrota)
 # Plotando Gráfico
 data = pd.DataFrame({
     
-    'x': ['VITÓRIA', 'DERROTA', 'EMPATE'],
-    'y': [vitoria, derrota, empate],
-}).set_index('x')
+     'x': ['VITÓRIA', 'DERROTA', 'EMPATE'],
+          'Resultado': [vitoria, derrota, empate],
+ }).set_index('x')
 st.bar_chart(data)
 
 # Aprensentando resultado final
